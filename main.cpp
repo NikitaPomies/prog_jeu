e46d072095de5eff3d7b4ba0b2f07fdfc1139a14
NikitaPomies <83075701+NikitaPomies@users.noreply.github.com>
2021-06-20 11:34:46 +0200

    mvmnt

--- main.cpp
+++ main.cpp
@@ -3,10 +3,29 @@
 using namespace Imagine;
 using namespace std;
 
+const int droite = 0;
+const int bottom = 1;
+const int gauche = 2;
+const int up = 3;
+
+
+struct point {
+    int x;
+    int y;
+    point operator+(point b) const;
+
+};
+
+point point::operator+(point b) const {
+    point p = {x+b.x,y+b.y};
+    return p;
+}
+const point dir[4] = {{1,0},{0,1},{-1,0},{0,-1}};
 
 
 
-int Clavier() {
+
+int keyboard() {
     Event e;
     do {
         getEvent(0,e);
@@ -18,16 +37,33 @@ int Clavier() {
 
 class Perso{
 public:
-    int xc,yc;
+
+    point c;
     int rayon;
-    void Dessine_perso();
+    Perso(int xc,int yc, int rayon);
+    void Dessine_perso(Color col);
+    void bouge(int d);
 
 };
 
-void Perso::Dessine_perso(){
+void Perso::Dessine_perso(Color col){
 
-    fillCircle(xc,yc,rayon,RED);
+
+    fillCircle(c.x,c.y,rayon,col);
+
 }
+
+Perso::Perso(int x,int y,int r){
+    c.x=x;
+    c.y=y;
+    rayon=r;
+
+
+
+
+}
+
+
 
 
 
@@ -36,6 +72,7 @@ class Bords{
 public:
     int xb,yb;
     void Dessine_bords();
+    Bords(int xb,int yb);
 
 
 
@@ -46,10 +83,61 @@ void Bords::Dessine_bords(){
     drawRect(0,0,xb,yb,BLACK);
 
 }
+Bords::Bords(int x,int y){
+
+    xb=x;
+    yb=y;
+}
+
+void Perso::bouge(int d){
+    Dessine_perso(WHITE);
+
+    c=c+dir[d];
+    Dessine_perso(RED);
+}
+
+
+
+void jeu(int w, int h){
+    Bords b(w,h);
+    Perso p(50,50,10);
+    b.Dessine_bords();
+    p.Dessine_perso(BLACK);
+    click();
+    cout<<"oui"<<endl;
+
+    do {
+        switch(keyboard()) {
+        case KEY_RIGHT:
+                 p.bouge(droite); break;
+        case KEY_DOWN:
+            p.bouge(bottom); break;
+        case KEY_LEFT:
+           p.bouge(gauche); break;
+        case KEY_UP:
+            p.bouge(up); break;
+        }
+        milliSleep(5);
+
+        }while(true);
+
+
+
+
+}
+
 
 
 
 int main(){
+
+    openComplexWindow(300,300);
+    //fillCircle(10,10,20,RED);
+    jeu(200,200);
+
+
+
+    endGraphics();
 
 
 
